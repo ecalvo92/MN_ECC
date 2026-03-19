c5068bb31d54451ee8679993e6ac33c100ee0796
Eduardo Calvo Castillo <ecalvo@crnova.com>
2026-03-18 21:04:42 -0600

    Update DataBase.sql

--- DataBase.sql
+++ DataBase.sql
@@ -18,6 +18,60 @@ USE `mn_db`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `tproducto`
+--
+
+DROP TABLE IF EXISTS `tproducto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tproducto` (
+  `Consecutivo` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(100) NOT NULL,
+  `Precio` decimal(10,2) NOT NULL,
+  `Descripcion` varchar(1000) NOT NULL,
+  `Cantidad` int(11) NOT NULL,
+  `Imagen` varchar(100) NOT NULL,
+  `Estado` bit(1) NOT NULL,
+  PRIMARY KEY (`Consecutivo`),
+  UNIQUE KEY `UK_Nombre` (`Nombre`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tproducto`
+--
+
+LOCK TABLES `tproducto` WRITE;
+/*!40000 ALTER TABLE `tproducto` DISABLE KEYS */;
+INSERT INTO `tproducto` VALUES (1,'Oxbow Essentials Adult Rabbit Food',1600.00,'El alimento Oxbow Essentials Adult Rabbit Food es un concentrado premium para conejos adultos, elaborado principalmente a base de heno de timothy. Se caracteriza por su alto contenido de fibra, lo que favorece una digestión saludable y ayuda al desgaste natural de los dientes.',8,'IMAGEN',_binary ''),(2,'Kaytee Forti-Diet Pro Health Hamster & Gerbil Food',1900.00,'El alimento Kaytee Forti-Diet Pro Health Hamster & Gerbil Food es un concentrado balanceado diseñado para cubrir las necesidades nutricionales de los hámsteres en todas sus etapas de vida. Está compuesto por una mezcla de granos, semillas, pellets y superalimentos como linaza, calabaza y espinaca, que contribuyen a una dieta variada y completa.',6,'IMAGEN',_binary '');
+/*!40000 ALTER TABLE `tproducto` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `trol`
+--
+
+DROP TABLE IF EXISTS `trol`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `trol` (
+  `Consecutivo` int(11) NOT NULL AUTO_INCREMENT,
+  `NombreRol` varchar(200) NOT NULL,
+  PRIMARY KEY (`Consecutivo`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `trol`
+--
+
+LOCK TABLES `trol` WRITE;
+/*!40000 ALTER TABLE `trol` DISABLE KEYS */;
+INSERT INTO `trol` VALUES (1,'Administrador'),(2,'Usuario');
+/*!40000 ALTER TABLE `trol` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `tusuario`
 --
 
@@ -31,9 +85,12 @@ CREATE TABLE `tusuario` (
   `Contrasenna` varchar(15) NOT NULL,
   `CorreoElectronico` varchar(100) NOT NULL,
   `Estado` bit(1) NOT NULL,
+  `ConsecutivoRol` int(11) NOT NULL,
   PRIMARY KEY (`Consecutivo`),
   UNIQUE KEY `UK_Identificacion` (`Identificacion`),
-  UNIQUE KEY `UK_CorreoElectronico` (`CorreoElectronico`)
+  UNIQUE KEY `UK_CorreoElectronico` (`CorreoElectronico`),
+  KEY `FK_Usuario_Rol` (`ConsecutivoRol`),
+  CONSTRAINT `FK_Usuario_Rol` FOREIGN KEY (`ConsecutivoRol`) REFERENCES `trol` (`Consecutivo`)
 ) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -43,7 +100,7 @@ CREATE TABLE `tusuario` (
 
 LOCK TABLES `tusuario` WRITE;
 /*!40000 ALTER TABLE `tusuario` DISABLE KEYS */;
-INSERT INTO `tusuario` VALUES (15,'119310359','BONILLA MIRANDA MAUREEM VALERIA','Saprissa','mbonilla10359@ufide.ac.cr',_binary ''),(16,'304590415','EDUARDO JOSE CALVO CASTILLO','90415','ecalvo90415@ufide.ac.cr',_binary ''),(17,'504420898','MARIN CHAVARRIA JIMENA MARIA','Prueba1234','jmarin20898@ufide.ac.cr',_binary '');
+INSERT INTO `tusuario` VALUES (15,'119310359','BONILLA MIRANDA MAUREEM VALERIA','12345678','mbonilla10359@ufide.ac.cr',_binary '',2),(16,'304590415','EDUARDO JOSE CALVO CASTILLO','90415','ecalvo90415@ufide.ac.cr',_binary '',1),(17,'504420898','MARIN CHAVARRIA JIMENA MARIA','12345678','jmarin20898@ufide.ac.cr',_binary '',2);
 /*!40000 ALTER TABLE `tusuario` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -69,6 +126,97 @@ BEGIN
 	UPDATE 	tusuario
     SET		Contrasenna = pNuevaContrasenna
 	WHERE 	Consecutivo = pConsecutivo;
+
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `sp_ActualizarPerfil` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_ZERO_IN_DATE,NO_ZERO_DATE,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_ActualizarPerfil`(	
+	pIdentificacion varchar(15),
+    pNombre varchar(200),
+    pCorreoElectronico varchar(100),
+    pConsecutivo int
+)
+BEGIN
+
+	UPDATE 	tusuario
+    SET		Identificacion = pIdentificacion,
+			Nombre = pNombre,
+            CorreoElectronico = pCorreoElectronico
+	WHERE 	Consecutivo = pConsecutivo;
+
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `sp_ConsultarProductos` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_ZERO_IN_DATE,NO_ZERO_DATE,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_ConsultarProductos`(
+   
+)
+BEGIN
+
+	SELECT 	Consecutivo,
+			Nombre,
+			Precio,
+			Descripcion,
+			Cantidad,
+			Imagen,
+			Estado,
+            CASE WHEN Estado = 1 THEN 'Activo' ELSE 'INACTIVO' END 'EstadoDescripcion'
+	FROM 	tproducto;
+
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `sp_ConsultarUsuario` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_ZERO_IN_DATE,NO_ZERO_DATE,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_ConsultarUsuario`(
+    pConsecutivo INT
+)
+BEGIN
+
+	SELECT  Consecutivo,
+			Identificacion,
+			Nombre,
+			CorreoElectronico,
+			Estado
+	FROM	tusuario
+    WHERE 	Consecutivo = pConsecutivo
+        AND Estado = 1;
 
 END ;;
 DELIMITER ;
@@ -126,8 +274,8 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_Registrar`(
 )
 BEGIN
 
-	INSERT INTO tusuario(Identificacion,Nombre,Contrasenna,CorreoElectronico,Estado)
-	VALUES (pIdentificacion, pNombre, pContrasenna,pCorreoElectroncio,1);
+	INSERT INTO tusuario(Identificacion,Nombre,Contrasenna,CorreoElectronico,Estado,ConsecutivoRol)
+	VALUES (pIdentificacion, pNombre, pContrasenna,pCorreoElectroncio,1,2);
 
 END ;;
 DELIMITER ;
@@ -175,4 +323,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-03-11 21:05:18
+-- Dump completed on 2026-03-18 21:04:28
